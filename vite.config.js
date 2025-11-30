8a8be7bb85a10c63e023d7b64e778940be7a6e57
Tekindar <116880404+Tekindar@users.noreply.github.com>
2025-11-30 20:02:32 +0800

    8

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
  * @type {import('vite').UserConfig}
  */
 export default {
-    base: process.env.NODE_ENV === 'production' ? '/Starship-Launching-and-Recycling/' : ''
+    base: process.env.NODE_ENV === 'production' ? 'Starship-Launching-and-Recycling/' : ''
 }
